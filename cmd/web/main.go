6948203acacc21b4d2178fd97467b4a1ebfe32d1
Benjamin Abarzua <abarzuabenjamin@gmail.com>
2024-01-02 13:33:36 -0300

    ignore gitignore

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
@@ -15,11 +16,19 @@ type application struct {
 func main() {
 	// flags
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
+	dsn := flag.String("dsn", "web:benja@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// logging
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// create app instance to inject loggers (dependencies) into the handler functions
 	app := &application{
@@ -36,6 +45,17 @@ func main() {
 
 	// main main
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
