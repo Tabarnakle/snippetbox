bb28cc18eba8f7fa5b58374840a0953ae2a294b8
Benjamin Abarzua <abarzuabenjamin@gmail.com>
2024-03-18 14:45:30 -0300

    add snippet create form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/tabarnakle/snippetbox/pkg/models"
 )
@@ -46,9 +48,37 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	title := "O snail"
-	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-	expires := "7"
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+
+	title := r.PostForm.Get("title")
+	content := r.PostForm.Get("content")
+	expires := r.PostForm.Get("expires")
+
+	errors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		errors["title"] = "This field cannot be blank."
+	} else if utf8.RuneCountInString(title) > 100 {
+		errors["title"] = "This field cannot exceed 100 characters."
+	}
+
+	if strings.TrimSpace(content) == "" {
+		errors["content"] = "This field cannot be blank."
+	}
+
+	if strings.TrimSpace(expires) == "" {
+		errors["expires"] = "This field cannot be blank."
+	} else if expires != "365" && expires != "7" && expires != "1" {
+		errors["expires"] = "This field is invalid."
+	}
+
+	if len(errors) > 0 {
+		fmt.Fprint(w, errors)
+	}
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
