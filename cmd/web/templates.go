e11286ab64c44e58b260f80b99e3b5ff0c25182e
Benjamin Abarzua <abarzuabenjamin@gmail.com>
2024-03-19 11:13:27 -0300

    refactor form methods into separate package

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/tabarnakle/snippetbox/pkg/forms"
 	"github.com/tabarnakle/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
